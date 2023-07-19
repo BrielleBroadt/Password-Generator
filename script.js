8579a598807fc9bfd4cd7ecf19e1dbc4dbdae57e
Brielle <brielleb.broadt@gmail.com>
2023-07-19 14:45:50 -0500

    added variables

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Assignment code here
+var numbers = "0123456789"
+var lowerCase ="abcdefghijklmnopqrstuvwxyz"
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var specialChar ="!@#$%^&*()_+"
+var newPassword = []
+var finalPassword = []
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+
+
+
+
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
