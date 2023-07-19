b850383f81a825a1eec5c15336869a8cc23b7c04
Brielle <brielleb.broadt@gmail.com>
2023-07-19 14:45:05 -0500

    added javascript file

---
+++ script.js
@@ -0,0 +1,9 @@
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  }
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  
